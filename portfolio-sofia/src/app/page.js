7ebb2d2f4975bab49f72d51445aa3720ec7bb699
Diogo Moreira <93798407+diogomufasa@users.noreply.github.com>
2023-06-07 08:48:56 +0000

    intial commit

---
+++ portfolio-sofia/src/app/page.js
@@ -0,0 +1,10 @@
+// Purpose: main page component
+import Navigation from './navigation';
+
+export default function Hero() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      {Navigation}
+    </main>
+  );
+}
