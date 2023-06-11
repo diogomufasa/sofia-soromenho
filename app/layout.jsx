c00bb67a56621ff15b0642353827ea82ea80b34e
Diogo Moreira <93798407+diogosupermufasa@users.noreply.github.com>
2023-06-11 13:14:04 +0100

    Developing

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import '@/styles/globals.css'
+import Nav from '@/components/Nav'
+import Footer from '@/components/Footer'
+
+export const metadata = {
+  title: 'Sofia Soromenho',
+  description: 'Sofia Soromenho - Dancer, Performer, Choreographer',
+  keywords: 'Sofia Soromenho, Dancer, Performer, Choreographer',
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"/>
+        </div>
+
+        <main className='app'>
+          <Nav />
+          {children}
+          <Footer />
+        </main>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout;
---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import Feed from "@components/Feed"
+
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Sofia Soromenho
+      <br className="max-md:hidden" />
+      <p className="desc text-center">Dancer</p>
+      </h1>
+    <Feed />
+    </section>
+  )
+}
+
+export default Home
