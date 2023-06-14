48c73d23ca9fe0bbca9f0d007d2d2f3ecfacab4f
Diogo Moreira <93798407+diogosupermufasa@users.noreply.github.com>
2023-06-14 17:12:29 +0100

    14/06/23

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,7 +5,19 @@ import { useState, useEffect } from "react";
 
 const Nav = () => {
   return (
-    <nav>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <div className="flex-between w-1/2">
+        <Link href="/" className="flex-between">
+            <Image
+              src="/assets/images/logo.png"
+              alt="logo"
+              width={50}
+              height={50}
+            />
+            <h1 className="ml-2 text-2xl font-bold uppercase">Sofia Soromenho</h1>
+        </Link>
+      </div>
+
     </nav>
 
   )
