d7faa873e9cbe47c14c90adf22364788c7abca53
Diogo Moreira <93798407+diogosupermufasa@users.noreply.github.com>
2023-06-28 03:04:24 +0100

    28/06/23

--- components/MediaNav.jsx
+++ components/MediaNav.jsx
@@ -3,12 +3,36 @@ import { useState, useEffect} from "react";
 import { X } from "lucide-react";
 
 const MediaNav = () => {
-    const [isToggled, setToggled] = useState(false);
+    const [isToggled, setToggled] = useState(true);
+
+    useEffect(() => {
+        if (isToggled) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "unset";
+        }
+    }, [isToggled]);
 
 
     return (
-        <div>
-        <X/>
+        <div className="media_nav__container">
+            <div onClick={() => setToggled(!isToggled)}>
+                <X size={24} />
+            </div>
+            <div className="media__links">
+                <Link href="/about" className="link-hover">
+                    About
+                </Link>
+                <Link href="/gallery" className="link-hover" >
+                    Gallery
+                </Link>
+                <Link href="/projects" className="link-hover">
+                    Projects
+                </Link>
+                <Link href="/blog" className="link-hover">
+                    Blog
+                </Link>
+            </div>
         </div>
     )
 }
