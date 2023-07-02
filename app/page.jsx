13da50376beaed66a0005c97be607d86c3681dd6
Diogo Moreira <93798407+diogosupermufasa@users.noreply.github.com>
2023-07-02 03:36:48 +0100

    02/07/23

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Feed from "@components/Feed"
 import Projects from "@components/Projects"
-import { Disc2 } from 'lucide-react';
+import { Disc2, ArrowRight } from 'lucide-react';
+import Link from "next/link";
 
 const Home = () => {
   return (
@@ -10,14 +11,16 @@ const Home = () => {
       <br className="max-md:hidden" />
       <p className="sub text-center">Dancer Teacher Choreogropher Phd</p>      
       </h1>
-    </section>
+    </section>    
     <div className='feed'>
+
       <span className="title"><Disc2 className="animate-pulse inline" color="#f45d5d"/> Current Project</span>
       <Feed />
     </div>  
     <div className="projects">
       <span className="title">Projects</span>  
     <Projects/>
+    <button className="outline_btn"><Link href="/projects">see all projects<ArrowRight className="inline" size={16}/> </Link></button>
     </div>
   </>
   )
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,11 +2,23 @@
 
 const Feed = () => {
   return ( 
-    <div className="cta">
-      <h1 className="head_text">Dance Museum</h1>
-      <p>Explore the history of dance</p>
+    <div className="card">
+      <h1 className="text1">Dance Museum</h1>
+      <p className="sub absolute pt-10">Portugal</p>
+      <div className="one">
+            <div className="two">
+                <div className="three">
+                    <div className="four">
+                        <div className="five">
+                          <p className="text2">An imersive contemporary dance experience. <br></br>Coming soon</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  )
+    );
 }
+
 
 export default Feed
